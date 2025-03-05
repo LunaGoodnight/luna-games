52c3f170189b800e57f018be704547614b1f47f3
Luna Goodnight <lunagoodnight@Lunas-MacBook-Pro.local>
2025-03-05 17:00:46 +0800

    add machine

---
+++ packages/game-dessert/src/GameManager.ts
@@ -0,0 +1,20 @@
+import layoutConfig from '../layoutConfig.json';
+import manifest from './manifest.json';
+export class GameManager {
+    layoutConfig;
+    constructor() {
+        this.layoutConfig = layoutConfig;
+    }
+
+    init(){
+
+    }
+
+    initializeGame() {
+
+    }
+
+    initializeStateMachine(){
+
+    }
+}
---
+++ packages/game-dessert/stateDiagram.ts
@@ -0,0 +1,17 @@
+import {setup} from "xstate";
+
+export interface IMachineContext {
+
+    soundOn: boolean;
+
+}
+export const dessertMachine = setup({
+ types: {
+     context: {} as IMachineContext,
+ }
+}).createMachine({
+    context: {
+        soundOn: true,
+
+    }
+})
